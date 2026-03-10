4d374e1a70a98c72db32e76c45701f330ab51d31
Denis Mikhin <digital.mikhin@gmail.com>
2026-03-10 13:41:13 +0300

    switch site to media.dmikhin.ru

--- deploy.bat
+++ deploy.bat
@@ -12,7 +12,7 @@ if errorlevel 1 (
 )
 
 echo [2/3] Upload to Yandex Object Storage...
-aws s3 sync ".\\out" s3://dmikhin-ru-media --endpoint-url https://storage.yandexcloud.net --profile yc --delete
+aws s3 sync ".\\out" s3://media.dmikhin.ru --endpoint-url https://storage.yandexcloud.net --profile yc --delete
 if errorlevel 1 (
   echo Upload failed.
   popd
@@ -20,7 +20,7 @@ if errorlevel 1 (
 )
 
 echo [2.1/3] Fix Content-Type for Decap config...
-aws s3 cp ".\\out\\admin\\config.yml" s3://dmikhin-ru-media/admin/config.yml --endpoint-url https://storage.yandexcloud.net --profile yc --content-type "text/yaml"
+aws s3 cp ".\\out\\admin\\config.yml" s3://media.dmikhin.ru/admin/config.yml --endpoint-url https://storage.yandexcloud.net --profile yc --content-type "text/yaml"
 if errorlevel 1 (
   echo Content-Type fix failed.
   popd
@@ -28,10 +28,10 @@ if errorlevel 1 (
 )
 
 echo [3/3] Verify bucket root...
-aws s3 ls s3://dmikhin-ru-media --endpoint-url https://storage.yandexcloud.net --profile yc
+aws s3 ls s3://media.dmikhin.ru --endpoint-url https://storage.yandexcloud.net --profile yc
 
 echo.
 echo Deploy finished.
-echo Check: https://dmikhin-ru-media.website.yandexcloud.net
+echo Check: http://media.dmikhin.ru
 popd
 endlocal
