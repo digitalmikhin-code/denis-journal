b838e13f3ea76ec9f57d9fd26d2ed3dab2462950
Denis Mikhin <digital.mikhin@gmail.com>
2026-04-06 15:26:57 +0300

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -22,8 +22,10 @@ function normalizeHttpUrl(value: string | undefined): string | null {
 
 export const REACTIONS_API_URL =
   normalizeHttpUrl(process.env.NEXT_PUBLIC_REACTIONS_API_URL) ?? DEFAULT_REACTIONS_API_URL;
+const DEFAULT_SUBSCRIBERS_API_URL =
+  "https://functions.yandexcloud.net/d4efmjusqpajbpmg5ckl";
 export const SUBSCRIBERS_API_URL =
-  normalizeHttpUrl(process.env.NEXT_PUBLIC_SUBSCRIBERS_API_URL) ?? null;
+  normalizeHttpUrl(process.env.NEXT_PUBLIC_SUBSCRIBERS_API_URL) ?? DEFAULT_SUBSCRIBERS_API_URL;
 export const YANDEX_METRIKA_ID = 107246735;
 
 export const CATEGORY_LABELS = {
