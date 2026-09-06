aaa6a0130dc2cc8fab561171aac379eb9ecd171c
digitalmikhin-code <digital.mikhin@gmail.com>
2026-09-06 22:30:36 +0300

    Wire analytics and HR conversion paths into journal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import Script from "next/script";
+import { Suspense } from "react";
+import { YandexMetrika } from "@/components/yandex-metrika";
+import { YANDEX_METRIKA_ID } from "@/lib/constants";
 import type { Metadata } from "next";
 import "./globals.css";
 import { SiteHeader } from "@/components/site-header";
@@ -119,9 +123,19 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body>
+        <Script id="yandex-metrika-init" strategy="beforeInteractive" dangerouslySetInnerHTML={{ __html: `
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+            k.async=1;k.src=r;a.parentNode.insertBefore(k,a);
+          })(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");
+          ym(${YANDEX_METRIKA_ID},"init",{defer:true,webvisor:true,clickmap:true,trackLinks:true,accurateTrackBounce:true,ecommerce:"dataLayer"});
+        ` }} />
+        <Suspense fallback={null}><YandexMetrika /></Suspense>
         <noscript>
           <div>
-            <img src="https://mc.yandex.ru/watch/107246735" style={{ position: "absolute", left: "-9999px" }} alt="" />
+            <img src={`https://mc.yandex.ru/watch/${YANDEX_METRIKA_ID}`} style={{ position: "absolute", left: "-9999px" }} alt="" />
           </div>
         </noscript>
         <script
